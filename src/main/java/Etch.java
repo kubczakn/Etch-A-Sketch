3c561b8faf8ea177dd07dbd289aeb082b93224f0
kubczakn <kubczakn@umich.edu>
2021-05-20 09:51:28 -0400

    drawing working on etch-a-sketch, basic clearing

--- src/main/java/Etch.java
+++ src/main/java/Etch.java
@@ -29,11 +29,11 @@ public class Etch
 
 // JPanel for image
 class MainPanel extends JPanel {
-//    private final BufferedImage img;
+    private final BufferedImage img;
     private final MovingAdapter ma = new MovingAdapter();
 //    private final KeyListener kl = new ArrowKey();
-    private final KeyListener kl = new ArrowListener();
-    private final JLabel label = new JLabel();
+//    private final KeyListener kl = new ArrowListener();
+    private final CustomLabel label = new CustomLabel();
     private List<Point> points = new ArrayList<Point>();
 
 
@@ -53,23 +53,26 @@ class MainPanel extends JPanel {
         y = 100;
 
         // Set focusable to be true
-        this.setFocusable(true);
-        this.requestFocusInWindow();
+//        this.setFocusable(true);
+//        this.requestFocusInWindow();
 
         // Set background color and size
         setBackground(Color.GRAY);
         setSize(800, 800);
 
         // Add Key Listener to JPanel
-        addKeyListener(kl);
+//        addKeyListener(kl);
 
         // Set image
-//        img = ImageIO.read(new File("src/main/resources/images/etch_a_sketch.jpg"));
-//        label.setIcon(new ImageIcon(img));
-//        label.setSize(label.getPreferredSize());
-//        label.addMouseListener(ma);
-//        label.addMouseMotionListener(ma);
-//        add(label);
+        img = ImageIO.read(new File("src/main/resources/images/etch_a_sketch.jpg"));
+        label.setIcon(new ImageIcon(img));
+        label.setSize(label.getPreferredSize());
+        label.addMouseListener(ma);
+        label.addMouseMotionListener(ma);
+        label.setFocusable(true);
+        label.requestFocus();
+        label.requestFocusInWindow();
+        add(label);
         setVisible(true);
     }
 
@@ -87,52 +90,138 @@ class MainPanel extends JPanel {
             int curr_y = point.getY();
             g2d.drawLine(curr_x, curr_y, curr_x, curr_y);
         }
+    }
 
+
+    class CustomLabel extends JLabel {
+        private KeyListener kl = new ArrowListener();
+        private int x;
+        private int y;
+
+        // TODO: Figure out how to clear points when shaken
+
+        private List<Point> points = new ArrayList<>();
+        public CustomLabel() {
+//            x = 400;
+//            y = 400;
+            x = -1;
+            y = -1;
+            addKeyListener(kl);
+//            requestFocus();
+        }
+
+        public void paintComponent(Graphics g) {
+            super.paintComponent(g);
+            drawLine(g);
+        }
+
+        public void drawLine(Graphics g) {
+            Graphics2D g2d = (Graphics2D)g;
+            g2d.setColor(Color.BLACK);
+            for (Point point : points) {
+                int curr_x = point.getX();
+                int curr_y = point.getY();
+                g2d.drawLine(curr_x, curr_y, curr_x, curr_y);
+            }
+        }
+        // Listener for arrow keys
+        private class ArrowListener implements KeyListener {
+            @Override public void keyTyped(KeyEvent e)
+            {
+
+            }
+
+            @Override
+            public void keyPressed(KeyEvent e) {
+                int key = e.getKeyCode();
+                if (x == -1) {
+                    x = getX() - 100;
+                    y = getY() - 120;
+                }
+                switch (key) {
+                case KeyEvent.VK_LEFT:
+                    x -= 1;
+                    points.add(new Point(x, y));
+                    System.out.println("Left!");
+                    repaint();
+                    break;
+                case KeyEvent.VK_RIGHT:
+                    x += 1;
+                    points.add(new Point(x, y));
+                    System.out.println("Right!");
+                    repaint();
+                    break;
+                case KeyEvent.VK_UP:
+                    y -= 1;
+                    points.add(new Point(x, y));
+                    System.out.println("Up!");
+                    repaint();
+                    break;
+                case KeyEvent.VK_DOWN:
+                    y += 1;
+                    points.add(new Point(x, y));
+                    System.out.println("Down!");
+                    repaint();
+                    break;
+                // Test clearing
+                case KeyEvent.VK_0:
+                    points.clear();
+                    System.out.println("Zero!");
+                    repaint();
+                    break;
+                }
+            }
+
+            @Override public void keyReleased(KeyEvent e)
+            {
+
+            }
+        }
     }
 
     // Listener for arrow keys
-    private class ArrowListener implements KeyListener {
-        @Override public void keyTyped(KeyEvent e)
-        {
-
-        }
-
-        @Override
-        public void keyPressed(KeyEvent e) {
-            int key = e.getKeyCode();
-            switch (key) {
-            case KeyEvent.VK_LEFT:
-                x -= 1;
-                points.add(new Point(x, y));
-                System.out.println("Left!");
-                repaint();
-                break;
-            case KeyEvent.VK_RIGHT:
-                x += 1;
-                points.add(new Point(x, y));
-                System.out.println("Right!");
-                repaint();
-                break;
-            case KeyEvent.VK_UP:
-                y -= 1;
-                points.add(new Point(x, y));
-                System.out.println("Up!");
-                repaint();
-                break;
-            case KeyEvent.VK_DOWN:
-                y += 1;
-                points.add(new Point(x, y));
-                System.out.println("Down!");
-                repaint();
-                break;
-            }
-        }
-
-        @Override public void keyReleased(KeyEvent e)
-        {
-
-        }
-    }
+//    private class ArrowListener implements KeyListener {
+//        @Override public void keyTyped(KeyEvent e)
+//        {
+//
+//        }
+//
+//        @Override
+//        public void keyPressed(KeyEvent e) {
+//            int key = e.getKeyCode();
+//            switch (key) {
+//            case KeyEvent.VK_LEFT:
+//                x -= 1;
+//                points.add(new Point(x, y));
+//                System.out.println("Left!");
+//                repaint();
+//                break;
+//            case KeyEvent.VK_RIGHT:
+//                x += 1;
+//                points.add(new Point(x, y));
+//                System.out.println("Right!");
+//                repaint();
+//                break;
+//            case KeyEvent.VK_UP:
+//                y -= 1;
+//                points.add(new Point(x, y));
+//                System.out.println("Up!");
+//                repaint();
+//                break;
+//            case KeyEvent.VK_DOWN:
+//                y += 1;
+//                points.add(new Point(x, y));
+//                System.out.println("Down!");
+//                repaint();
+//                break;
+//            }
+//        }
+//
+//        @Override public void keyReleased(KeyEvent e)
+//        {
+//
+//        }
+//    }
 
     // Logic for moving image
     class MovingAdapter extends MouseAdapter
